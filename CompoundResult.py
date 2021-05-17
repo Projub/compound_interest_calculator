a1db3e01e04a8e125cd883fe4e0e5b8a8e4ada53
Robin <robin_boone@live.be>
2021-05-17 20:18:56 +0200

    generate_compound_possibilities to use dynamic list of Periods

--- CompoundResult.py
+++ CompoundResult.py
@@ -3,7 +3,7 @@ class CompoundResult:
     for i in range(101):
         monthly_returns[i] = (1 + (i/100)) ** (1 / float(12))
 
-    def __init__(self, parent, annual_return, months, monthly_deposit, initial_amount=None):
+    def __init__(self, parent, annual_return, months, monthly_deposit=0, initial_amount=None):
         self.parent = parent
         self.annual_return = annual_return
         self.months = months
@@ -31,44 +31,38 @@ class CompoundResult:
         return results
 
 
+class Period:
+    def __init__(self, months, monthly_deposit=0):
+        self.months = months
+        self.monthly_deposit = monthly_deposit
+
+
 def make_initial_compoundresult(initial_amount):
     return CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=None, initial_amount=initial_amount)
 
 
-max_rate = 16
+def generate_compound_possibilities(initial_amount, periods, min_rate, max_rate):
+    base = [make_initial_compoundresult(initial_amount=initial_amount)]
+    results = []
+    for p in periods:
+        for res in base:
+            for r in range(min_rate, max_rate+1):
+                results.append(CompoundResult(parent=res, annual_return=r, months=p.months, monthly_deposit=p.monthly_deposit))
+        base = results
+        if p != periods[-1]:
+            results = []
 
-init = make_initial_compoundresult(initial_amount=20000)
-
-period1 = []
-for r in range(max_rate):
-    period1.append(CompoundResult(parent=init, annual_return=r, monthly_deposit=1250, months=24))
-
-period2 = []
-for res in period1:
-    for r in range(max_rate):
-        period2.append(CompoundResult(parent=res, annual_return=r, monthly_deposit=1000, months=50))
-
-period3 = []
-for res in period2:
-    for r in range(max_rate):
-        period3.append(CompoundResult(parent=res, annual_return=r, monthly_deposit=0, months=75))
-
-period4 = []
-for res in period3:
-    for r in range(max_rate):
-        period4.append(CompoundResult(parent=res, annual_return=r, monthly_deposit=0, months=75))
-
-period5 = []
-for res in period4:
-    for r in range(max_rate):
-        period5.append(CompoundResult(parent=res, annual_return=r, monthly_deposit=0, months=75))
-
-for res in period5:
-    compound_results = res.get_compoundresults()
-    row = str(compound_results[0].result)
-    for i in range(1, len(compound_results)):
-        row = row + f" --{compound_results[i].annual_return}--> {compound_results[i].result}"
-    print(row)
+    for res in results:
+        compound_results = res.get_compoundresults()
+        row = str(compound_results[0].result)
+        for i in range(1, len(compound_results)):
+            row = row + f" --{compound_results[i].annual_return}--> {compound_results[i].result}"
+        print(row)
 
 
-
+# testing the code:
+test_periods = [Period(months=24, monthly_deposit=1250), Period(months=50, monthly_deposit=1000), Period(months=75), Period(months=75), Period(months=75)]
+test_min_rate = 11
+test_max_rate = 11
+test_initial_amount = 20000
+generate_compound_possibilities(test_initial_amount, test_periods, test_min_rate, test_max_rate)
