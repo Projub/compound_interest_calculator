74cf4e434eafb36604d520f8036e26f8d7dfe421
Robin Boone <robin_boone@live.be>
2021-07-20 22:21:08 +0000

    easier average compoundresult rows

--- CompoundResult.py
+++ CompoundResult.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 class CompoundResult:
     monthly_returns = [None]*101
     for i in range(101):
@@ -21,7 +23,7 @@ class CompoundResult:
     def has_parent(self):
         return self.parent is not None
 
-    def get_compoundresults(self):
+    def get_compound_results(self):
         results = []
         pointer = self
         results.insert(0, pointer)
@@ -43,40 +45,47 @@ class Period:
         return months
 
 
-def make_initial_compoundresult(initial_amount):
-    return CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=None, initial_amount=initial_amount)
+###### Not user friendly / results would get too big for excel
+# def generate_compound_possibilities(initial_amount, periods, min_rate, max_rate):
+#     base = [CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=0, initial_amount=initial_amount)]
+#     results = []
+#     for p in periods:
+#         for res in base:
+#             for r in range(min_rate, max_rate+1):
+#                 results.append(CompoundResult(parent=res, annual_return=r, months=p.months, monthly_deposit=p.monthly_deposit))
+#         base = results
+#         if p != periods[-1]:
+#             results = []
+#
+#     for res in results:
+#         compound_results = res.get_compound_results()
+#         row = str(compound_results[0].result)
+#         for i in range(1, len(compound_results)):
+#             row = row + f" --{compound_results[i].annual_return}--> {compound_results[i].result}"
+#         print(row)
 
 
-def generate_compound_possibilities(initial_amount, periods, min_rate, max_rate):
-    base = [make_initial_compoundresult(initial_amount=initial_amount)]
-    results = []
+def generate_compound_row(initial_amount, periods, avg_return):
+    last_compound_result = CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=0, initial_amount=initial_amount)
     for p in periods:
-        for res in base:
-            for r in range(min_rate, max_rate+1):
-                results.append(CompoundResult(parent=res, annual_return=r, months=p.months, monthly_deposit=p.monthly_deposit))
-        base = results
-        if p != periods[-1]:
-            results = []
+        last_compound_result = CompoundResult(parent=last_compound_result, annual_return=avg_return, months=p.months, monthly_deposit=p.monthly_deposit)
+    results = last_compound_result.get_compound_results()
 
+    row = []
     for res in results:
-        compound_results = res.get_compoundresults()
-        row = str(compound_results[0].result)
-        for i in range(1, len(compound_results)):
-            row = row + f" --{compound_results[i].annual_return}--> {compound_results[i].result}"
-        print(row)
+        row.append(f"{res.result} ({res.annual_return})")
+    print(row)
 
 
 # testing the code:
 test_periods = [Period(months=24, monthly_deposit=1250), Period(months=50, monthly_deposit=1000), Period(months=75), Period(months=75), Period(months=75)]
-test_min_rate = 11
-test_max_rate = 11
-test_initial_amount = 20000
-generate_compound_possibilities(test_initial_amount, test_periods, test_min_rate, test_max_rate)
+for avg_return in range(26):
+    generate_compound_row(20000, test_periods, avg_return)
 
-print("goal amount printed for every month:")
-goal_monthly = []
-for period in test_periods:
-    temp_periods = period.return_monthly_periods()
-    for month in temp_periods:
-        goal_monthly.append(month)
-generate_compound_possibilities(test_initial_amount, goal_monthly, test_min_rate, test_max_rate)
+# print("goal amount printed for every month:")
+# goal_monthly = []
+# for period in test_periods:
+#     temp_periods = period.return_monthly_periods()
+#     for month in temp_periods:
+#         goal_monthly.append(month)
+# generate_compound_possibilities(test_initial_amount, goal_monthly, test_min_rate, test_max_rate)
