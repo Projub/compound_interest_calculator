a9fe334f1cf19e227ff6fe3e27f7f3e2e5c216da
Robin Boone <robin_boone@live.be>
2021-07-21 15:35:52 +0000

    dataframe concept with monthly columns and even return rows

--- CompoundResult.py
+++ CompoundResult.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+import datetime
+
+import numpy as np
+import pandas as pd
+import datetime as dt
+
 
 class CompoundResult:
     monthly_returns = [None]*101
@@ -26,10 +31,10 @@ class CompoundResult:
     def get_compound_results(self):
         results = []
         pointer = self
-        results.insert(0, pointer)
+        results.insert(0, pointer.result)
         while pointer.has_parent():
             pointer = pointer.parent
-            results.insert(0, pointer)
+            results.insert(0, pointer.result)
         return results
 
 
@@ -45,42 +50,56 @@ class Period:
         return months
 
 
-###### Not user friendly / results would get too big for excel
-# def generate_compound_possibilities(initial_amount, periods, min_rate, max_rate):
-#     base = [CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=0, initial_amount=initial_amount)]
-#     results = []
-#     for p in periods:
-#         for res in base:
-#             for r in range(min_rate, max_rate+1):
-#                 results.append(CompoundResult(parent=res, annual_return=r, months=p.months, monthly_deposit=p.monthly_deposit))
-#         base = results
-#         if p != periods[-1]:
-#             results = []
-#
-#     for res in results:
-#         compound_results = res.get_compound_results()
-#         row = str(compound_results[0].result)
-#         for i in range(1, len(compound_results)):
-#             row = row + f" --{compound_results[i].annual_return}--> {compound_results[i].result}"
-#         print(row)
+# Not used: user friendly / results would get too big for excel file
+def generate_compound_possibilities(initial_amount, periods, min_rate, max_rate):
+    base = [CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=0, initial_amount=initial_amount)]
+    results = []
+    for p in periods:
+        for res in base:
+            for r in range(min_rate, max_rate+1):
+                results.append(CompoundResult(parent=res, annual_return=r, months=p.months, monthly_deposit=p.monthly_deposit))
+        base = results
+        if p != periods[-1]:
+            results = []
+
+    for res in results:
+        compound_results = res.get_compound_results()
+        row = str(compound_results[0].result)
+        for i in range(1, len(compound_results)):
+            row = row + f" --{compound_results[i].annual_return}--> {compound_results[i].result}"
+        print(row)
 
 
-def generate_compound_row(initial_amount, periods, avg_return):
+def generate_compound_result_array(initial_amount, periods, avg_return):
     last_compound_result = CompoundResult(parent=None, annual_return=None, months=None, monthly_deposit=0, initial_amount=initial_amount)
     for p in periods:
         last_compound_result = CompoundResult(parent=last_compound_result, annual_return=avg_return, months=p.months, monthly_deposit=p.monthly_deposit)
     results = last_compound_result.get_compound_results()
-
-    row = []
-    for res in results:
-        row.append(f"{res.result} ({res.annual_return})")
-    print(row)
+    # row = []
+    # for res in results:
+    #     row.append(f"{res.result} ({res.annual_return})")
+    # print(row)
+    return results
 
 
 # testing the code:
-test_periods = [Period(months=24, monthly_deposit=1250), Period(months=50, monthly_deposit=1000), Period(months=75), Period(months=75), Period(months=75)]
-for avg_return in range(26):
-    generate_compound_row(20000, test_periods, avg_return)
+periods = [Period(months=24, monthly_deposit=1250), Period(months=50, monthly_deposit=1000), Period(months=75), Period(months=75), Period(months=75)]
+periods_months = []
+given_date = '01/09/2020'
+date = dt.datetime.strptime(given_date, '%d/%m/%Y')
+dates = [date]
+for p in periods:
+    periods_months = periods_months + p.return_monthly_periods()
+for m in periods_months:
+    date = date + pd.DateOffset(months=1)
+    dates.append(date)
+
+df = pd.DataFrame([], columns=dates)
+for ret in range(26):
+    df = df.append(pd.DataFrame([generate_compound_result_array(20000, periods_months, ret)], columns=dates))
+
+print(df)
+
 
 # print("goal amount printed for every month:")
 # goal_monthly = []
